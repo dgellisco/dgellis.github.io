637230a99139928d40f3c4775f298596e4d42af3
dgellis <43307996+dgellisco@users.noreply.github.com>
2018-10-25 13:03:13 -0600

    challenging

--- 04-JQuery-RPG/assets/javascript/game.js
+++ 04-JQuery-RPG/assets/javascript/game.js
@@ -1,6 +1,7 @@
+// DONT FORGET TO REVEAL SECRET CHARACTER IF DONALD 04 IS DEFEATED
 
-var WarriorsObj = [
-    {
+
+var WarriorsObj = [{
         id: "01",
         name: "George W. Bush",
         hp: 100,
@@ -8,7 +9,7 @@ var WarriorsObj = [
         cp: 10,
         phrases: [
             "I want to be president.  Being president was FUN.",
-            "Hello?",
+            "phrase2",
             "phrase3",
         ],
     },
@@ -44,7 +45,7 @@ var WarriorsObj = [
         cp: 25,
         phrases: [
             "This is going to be HUGE!",
-            "This will make America Great Again!",
+            "By God, I will make America great again!",
         ]
     },
     {
@@ -54,13 +55,14 @@ var WarriorsObj = [
         ap: 4,
         cp: 10,
         phrases: [
+            "For Mother Russia!",
             "You'll never stop me",
-            "For the Mother Land!",
+            "Don't make me take my shirt off",
         ]
     },
 ];
 
-var totalAttacks = 0;
+var attackMultiplier = 0;
 
 var chooseWarrior = true;
 var heroIndex = "";
@@ -78,74 +80,86 @@ var statusDiv = document.getElementById("status-display");
 var statusDisplay;
 
 
-// Document ready
-$(document).ready(function() {
+// Start
+$(document).ready(function () {
     newGame();
     $(".warriors").css("cursor", "pointer");
 
-if (chooseWarrior == true) {
-    $(".warriors").click(function() {
-        newWarrior();
-        chooseWarrior = false;
-        chooseEnemy = true;
-    });
-}
+    // Depending on game state/stage, clicking on a picture (i.e. anything in "warriors" class) will run a different function.
+    // Done in reverse so as to not run twice.
+    $(".warriors").click(function () {
 
-else if (chooseEnemy == true) {
-    $(".warriors").click(function() {
-        newEnemy();
-        chooseEnemy = false;
-        gameStarted = true;
-    });
-}
+        if (gameStarted == true) {
+            console.log("Console: You are about to attack an enemy!");
+            newAttack();
+        }
 
-else if (gameStarted == true) {
-    $(".warriors").click(function() {
-        newRound();
-    });
-}
+        if (chooseEnemy == true) {
+            enemyIndex = $(this).attr("id");
+            enemyIndex = enemyIndex.replace("warrior", "");
+            console.log("Enemy ID = " + enemyIndex);
 
-else {
-    alert("If/Else statement end reached");
-}
+            console.log("Console: You clicked on an enemy!");
+            newEnemy();
+        }
+
+        else if (chooseWarrior == true) {
+            heroIndex = $(this).attr("id");
+            heroIndex = heroIndex.replace("warrior", "");
+            console.log("Hero ID = " + heroIndex);
+
+            console.log("Console: You clicked on a warrior");
+            newWarrior();
+            
+        }
+
+        else {
+            alert("End of if/else click statement");
+        }
+
+    });
 
 });
 
-// function updateWarriors() {
-//     $("#warriors-available").append('\
-//     <span class="warriors card text-center bg-light" id="warrior' + WarriorsObj[index].id + '">\
-//         <div class="card-header">\
-//             <span id="warrior' + WarriorsObj[index].id + '-name">' 
-//             + WarriorsObj[index].name + '</span>\
-//         </div>\
-//         <div class="card-body">\
-//             <img src="assets/images/hero' + WarriorsObj[index].id + '.jpg" alt="Candidate" id="warrior' + WarriorsObj[index].id + '-pic">\
-//         </div>\
-//         <div class="card-footer text-muted">\
-//             HP: ' + WarriorsObj[index].hp + '\
-//             <span id="warrior' + WarriorsObj[index].id + '-hp"></span>\
-//         </div>\
-//     </span>\
-//     ');
-// }
 
+// FUNCTION JUNCTION FUNCTION JUNCTION FUNCTION JUNCTION FUNCTION JUNCTION FUNCTION JUNCTION
+// FUNCTION JUNCTION FUNCTION JUNCTION FUNCTION JUNCTION FUNCTION JUNCTION FUNCTION JUNCTION
+
+// Setup Step 1: Populate characters at start
 function newGame() {
-    // POPULATE CHARACTERS AT START
-    $(WarriorsObj).each(function(index) {
+    $(WarriorsObj).each(function (index) {
         console.log("Console: Populating html with characters");
+        warriorID = WarriorsObj[index].id;
 
-        // IGNORE SECRET CHARACTER
-        if(index === WarriorsObj.length - 1) {
+        // HIDE PUTIN... Get him working behind the scenes ;).  This is important because we need to create the element here and now.
+        // We can then use it later.
+        if (index === WarriorsObj.length - 1) {
+            $("#warriors-available").append('\
+            <span class="warriors card text-center bg-light" id="warrior' + WarriorsObj[index].id + '">\
+                <div class="card-header">\
+                    <span id="warrior' + WarriorsObj[index].id + '-name">' +
+                WarriorsObj[index].name + '</span>\
+                </div>\
+                <div class="card-body">\
+                    <img src="assets/images/hero' + WarriorsObj[index].id + '.jpg" alt="Candidate" id="warrior' + WarriorsObj[index].id + '-pic">\
+                </div>\
+                <div class="card-footer text-muted">\
+                    HP: ' + WarriorsObj[index].hp + '\
+                    <span id="warrior' + WarriorsObj[index].id + '-hp"></span>\
+                </div>\
+            </span>\
+            ');
+            console.log("Vladimir ID is " + warriorID);
+            $("#warrior" + warriorID).hide();
             return;
         }
 
-        warriorID = WarriorsObj[index].id;
-        
+        // Add all other candidates
         $("#warriors-available").append('\
         <span class="warriors card text-center bg-light" id="warrior' + WarriorsObj[index].id + '">\
             <div class="card-header">\
-                <span id="warrior' + WarriorsObj[index].id + '-name">' 
-                + WarriorsObj[index].name + '</span>\
+                <span id="warrior' + WarriorsObj[index].id + '-name">' +
+            WarriorsObj[index].name + '</span>\
             </div>\
             <div class="card-body">\
                 <img src="assets/images/hero' + WarriorsObj[index].id + '.jpg" alt="Candidate" id="warrior' + WarriorsObj[index].id + '-pic">\
@@ -156,36 +170,35 @@ function newGame() {
             </div>\
         </span>\
         ');
+
     });
     $("#game-status").html("Select a candidate.");
     console.log("newGame() executed succesfully");
+    console.log("chooseWarrior is " + chooseWarrior);
+    console.log("chooseEnemy is " + chooseEnemy);
+    console.log("gameStarted is " + gameStarted);
 }
-    
+
+
+// Setup Step 2: Pick your character
 function newWarrior() {
-    // CHOOSE YOUR WARRIOR
-    $(WarriorsObj).each(function(index) {
-        console.log("Console: You clicked on a warrior");
-        
-        var heroIndex = $(this).attr("id");
-        heroIndex = heroIndex.replace("warrior", "");
-        console.log("Hero ID = " + heroIndex);
+    console.log("newWarrior function just started");
 
-        $(WarriorsObj).each(function(index) {
-            
-            if(index === WarriorsObj.length - 1) {
-                return;
-            }           
-            
-            $("#warriors-available").empty();
+    $(WarriorsObj).each(function (index) {
 
-            console.log("heroIndex is " + heroIndex);
-            if(heroIndex == WarriorsObj[index].id) {
-                console.log("heroIndex is " + heroIndex);
-                $("#warriors-attacker").html('\
+        if (index === WarriorsObj.length - 1) {
+            return;
+        }
+
+        $("#warriors-available").empty();
+
+        console.log("heroIndex is " + heroIndex);
+        if (heroIndex == WarriorsObj[index].id) {
+            $("#warriors-attacker").html('\
                 <span class="warriors card text-center bg-light" id="warrior' + WarriorsObj[index].id + '">\
                     <div class="card-header">\
-                        <span id="warrior' + WarriorsObj[index].id + '-name">' 
-                        + WarriorsObj[index].name + '</span>\
+                        <span id="warrior' + WarriorsObj[index].id + '-name">' +
+                WarriorsObj[index].name + '</span>\
                     </div>\
                     <div class="card-body">\
                         <img src="assets/images/hero' + WarriorsObj[index].id + '.jpg" alt="Candidate" id="warrior' + WarriorsObj[index].id + '-pic">\
@@ -196,14 +209,14 @@ function newWarrior() {
                     </div>\
                 </span>\
                 ');
-            }
+        }
 
-            if(heroIndex !== WarriorsObj[index].id) {
-                $("#warriors-enemies").append('\
+        if (heroIndex !== WarriorsObj[index].id) {
+            $("#warriors-enemies").append('\
                 <span class="warriors card text-center bg-light" id="warrior' + WarriorsObj[index].id + '">\
                     <div class="card-header">\
-                        <span id="warrior' + WarriorsObj[index].id + '-name">' 
-                        + WarriorsObj[index].name + '</span>\
+                        <span id="warrior' + WarriorsObj[index].id + '-name">' +
+                WarriorsObj[index].name + '</span>\
                     </div>\
                     <div class="card-body">\
                         <img src="assets/images/hero' + WarriorsObj[index].id + '.jpg" alt="Candidate" id="warrior' + WarriorsObj[index].id + '-pic">\
@@ -214,41 +227,86 @@ function newWarrior() {
                     </div>\
                 </span>\
                 ');
-            }
+        }
 
-        });
     });
-    console.log("newWarrior() executed succesfully")
+    console.log("newWarrior() executed succesfully");
+    chooseWarrior = false;
+    chooseEnemy = true;
+    console.log("chooseWarrior is " + chooseWarrior);
+    console.log("chooseEnemy is " + chooseEnemy);
+    console.log("gameStarted is " + gameStarted);
 }
 
 
+// Setup Step 3: Pick your enemy
+function newEnemy() {
 
-    // <!-- newLevel() -->
-// newLevel()
-        // <!-- select warrior[i] -->
-        // <!-- other warriors become defenders[-] -->
-        
+    console.log("newEnemy function just started");
+    console.log("enemyIndex is " + enemyIndex);
 
-            // <!-- newEnemy() -->
-                // <!-- pick enemy, move to bottom -->
-                // Putin not available
+    $("#warriors-enemies").empty();
 
-                // <!-- attackEnemy() -->
-                    // <!-- on button click, attack -->
-                    $("#btn-attack").on("click", function() {    // dollar sign, selector, event, function                       
-                      });
-                    // <!-- warrior[i] attacks defender for AP*TotalAttacks -->
-                    // <!-- update defender HP -->
-                        // <!-- if defenders[i] has remaining HP -->
-                            // <!-- attack back for CP -->
-                            // <!-- update attacker HP -->
-                            // <!-- if attacker hp >0 -->
-                                // <!-- allow attacking again -->
-                            // <!-- if attack hp <=0 -->
-                                // <!-- lose screen -->
-                        // <!-- if defender[i] does not have remaining HP, do not attack back -->
-                            // <!-- remove defender     -->
-                            // if trump, add in Putin
-                            // <!-- if enemies remaining   -->
-                                // <!-- allow chosing of new enemy -->
-                            // <!-- if no enemies remaining, go to win screen -->
+    $(WarriorsObj).each(function (index) {
+
+        if (index === WarriorsObj.length - 1) {
+            return;
+        }
+
+        console.log("enemyIndex is " + enemyIndex);
+        if (enemyIndex == WarriorsObj[index].id) {
+            $("#warriors-defender").html('\
+                <span class="warriors card text-center bg-light" id="warrior' + WarriorsObj[index].id + '">\
+                    <div class="card-header">\
+                        <span id="warrior' + WarriorsObj[index].id + '-name">' +
+                WarriorsObj[index].name + '</span>\
+                    </div>\
+                    <div class="card-body">\
+                        <img src="assets/images/hero' + WarriorsObj[index].id + '.jpg" alt="Candidate" id="warrior' + WarriorsObj[index].id + '-pic">\
+                    </div>\
+                    <div class="card-footer text-muted">\
+                        HP: ' + WarriorsObj[index].hp + '\
+                        <span id="warrior' + WarriorsObj[index].id + '-hp"></span>\
+                    </div>\
+                </span>\
+                ');
+        }
+
+        if (enemyIndex !== WarriorsObj[index].id) {
+            $("#warriors-enemies").append('\
+                <span class="warriors card text-center bg-light" id="warrior' + WarriorsObj[index].id + '">\
+                    <div class="card-header">\
+                        <span id="warrior' + WarriorsObj[index].id + '-name">' +
+                WarriorsObj[index].name + '</span>\
+                    </div>\
+                    <div class="card-body">\
+                        <img src="assets/images/hero' + WarriorsObj[index].id + '.jpg" alt="Candidate" id="warrior' + WarriorsObj[index].id + '-pic">\
+                    </div>\
+                    <div class="card-footer text-muted">\
+                        HP: ' + WarriorsObj[index].hp + '\
+                        <span id="warrior' + WarriorsObj[index].id + '-hp"></span>\
+                    </div>\
+                </span>\
+                ');
+        }
+
+    });
+    console.log("newEnemy() executed succesfully")
+    chooseEnemy = false;
+    gameStarted = true;
+    console.log("chooseWarrior is " + chooseWarrior);
+    console.log("chooseEnemy is " + chooseEnemy);
+    console.log("gameStarted is " + gameStarted);
+}
+
+
+// Game On: Attack enemy on click
+// You attack.  Enemy HP = HP - (AP * attackMultiplier)
+    // If enemy HP > 0, enemy attacks back.  Hero HP = HP - CP.  Once done,
+        // if Hero HP > 0, you can attack again.
+        // if Hero HP <= 0, lose screen
+    // If enemy HP <= 0, enemy defeated (hide).
+        // enemiesRemaining--
+        // If enemy is trump and defeated, gets replaced with Putin, start over
+        // if enemiesRemaining >1, you select a new enemy (chooseEnemy = true)
+        // if enemiesRemaining == 0, you win
