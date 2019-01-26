f2109184b6cabe6be4caf68573d726386de4ac05
Dave Ellis <dgellis@live.com>
2019-01-26 13:35:41 -0700

    Express Assignment

---
+++ 13-Express/FriendFinder/app/data/friends.js
@@ -0,0 +1,76 @@
+var friendsList = [
+    {
+        name: "Boba Fett",
+        picture: "https://media.giphy.com/media/D6C9xVqgMI5yg/giphy.gif",
+        scores: [
+            4,
+            3,
+            4,
+            1,
+            1,
+            2
+        ]
+    },
+    {
+        name: "Darth Vader",
+        picture: "https://thumbs.gfycat.com/ImprobableGlumHarrier-size_restricted.gif",
+        scores: [
+            5,
+            5,
+            5,
+            2,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Luke Skywalker",
+        picture: "https://i1.wp.com/media.giphy.com/media/3owzVUrjJfcBXyQLkc/giphy.gif?w=620&ssl=1",
+        scores: [
+            1,
+            1,
+            5,
+            1,
+            5,
+            2
+        ]
+    },
+    {
+        name: "Wicket The Ewok",
+        picture: "https://media.giphy.com/media/8ccWKINky7tAAuEyk3/giphy.gif",
+        scores: [
+            2,
+            2,
+            3,
+            4,
+            3,
+            4
+        ]
+    },
+    {
+        name: "Jar Jar Binks",
+        picture: "https://thumbs.gfycat.com/MedicalYearlyAustraliankestrel-small.gif",
+        scores: [
+            3,
+            2,
+            3,
+            3,
+            3,
+            5
+        ]
+    },
+    {
+        name: "R2D2",
+        picture: "https://i.imgur.com/IsBHReF.gif",
+        scores: [
+            2,
+            2,
+            4,
+            5,
+            4,
+            4
+        ]
+    }
+];
+
+module.exports = friendsList;
